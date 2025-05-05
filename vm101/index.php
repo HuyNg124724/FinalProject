5e1b8ab18a52fad5ade507cb1b2231f5520064f6
Annnnay <maythukhanaing99@gmail.com>
2025-05-05 17:37:14 +0700

    Upload project files from VM101

---
+++ vm101/index.php
@@ -0,0 +1,40 @@
+<?php
+$videos = [
+  "Music Video" => "QmUeg6X9qMVJJzjzgsKt9eVLDnSPW53BvmCCZeYKbUat7M"
+];
+?>
+<h2>Stored Videos</h2>
+<ul>
+<?php foreach($videos as $title => $cid): ?>
+  <li>
+    <?php echo htmlspecialchars($title); ?> -
+    <a href="play.php?cid=<?php echo urlencode($cid); ?>">Play</a> |
+    <a href="http://192.168.50.101:8080/ipfs/<?php echo $cid; ?>" target="_blank">IPFS Link</a>
+  </li>
+<?php endforeach; ?>
+</ul>
+<h2>Live Stream</h2>
+<video id="liveVideo" controls autoplay width="640" height="360"></video>
+<script src="/hls.js"></script>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('liveVideo');
+    var videoSrc = 'http://192.168.50.101/stream.m3u8';
+    if (Hls.isSupported()) {
+        var hls = new Hls();
+        hls.loadSource(videoSrc);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, function() {
+            video.play();
+        });
+    } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        video.src = videoSrc;
+        video.addEventListener('loadedmetadata', function() {
+            video.play();
+        });
+    } else {
+        video.outerHTML = '<div>Your browser does not support HLS live streaming.</div>';
+    }
+});
+</script>
+
