a01df1b4a453dcc41fa92966011c605dd418db3b
HuyNg124724 <st124724@ait.asia>
2025-05-05 11:54:03 +0000

    Upload project files from VM102 including Apache configs

---
+++ play.php
@@ -0,0 +1,8 @@
+<?php
+$cid = $_GET['cid'];
+?>
+<h2>Playing Video</h2>
+<video controls autoplay width="640">
+  <source src="http://192.168.50.102:8080/ipfs/<?php echo htmlspecialchars($cid); ?>" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
